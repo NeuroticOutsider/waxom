4d0528a3de2ddbd3a2db0b45508ceb7133a59630
NeuroticOutsider <klimberg-nikita@yandex.ru>
2019-11-27 23:06:25 +0300

    Header done + adaptive

--- js/common.js
+++ js/common.js
@@ -1,20 +1,26 @@
 $(function() {
 
-  $('.hamburger').on('click', function () {
+  $('.hamburger').click(function () {
 		$(this).toggleClass('is-active');
-		// $('.navbar').toggleClass('navbar--show');
+		if($(this).hasClass('is-active')) {
+			$('.navbar__list-item').slideDown();
+			$('.body').addClass('body--active');
+		} else {
+			$('.navbar__list-item').slideUp();
+			$('.body').removeClass('body--active');
+		};
 	});
 
 	// top slider
 
-// 	$('.top-carousel').owlCarousel({
-// 		loop: true,
-// 		items: 1,
-// 		nav: true,
-// 		smartSpeed: 700,
-// 		responsiveClass: true,
-// 		dots: true, 
-// 		autoplay: true,
-//     autoplayTimeout: 3000
-// 	});
+	$('.top-carousel').owlCarousel({
+		loop: true,
+		items: 1,
+		nav: true,
+		smartSpeed: 700,
+		responsiveClass: true,
+		dots: true, 
+		autoplay: true,
+    autoplayTimeout: 5000
+	});
 });
