9de145024e89f4e72853e5f13e61f96685d043ca
NeuroticOutsider <klimberg-nikita@yandex.ru>
2019-11-29 04:00:30 +0300

    Filter and load-more button in the section projects done

--- js/common.js
+++ js/common.js
@@ -11,7 +11,7 @@ $(function() {
 		};
 	});
 
-	// top slider
+	// top slider (header)
 
 	$('.top-carousel').owlCarousel({
 		loop: true,
@@ -23,4 +23,59 @@ $(function() {
 		autoplay: true,
     autoplayTimeout: 10000
 	});
+
+	// Filter and load-more button (section projects)
+
+	$('.projects__list-btn').click(function() {
+		$('.projects__list-btn').removeClass('active');
+		$(this).addClass('active');
+	});
+
+	var $projectsItem = $('.projects__list-item').hide(),
+			viewLess = $('.projects__view-less').hide(),
+			$curr;
+
+  $('.projects__list-btn').click(function() {
+		$curr = $projectsItem.filter('.' + this.id).hide();
+    $curr.slice(0, 6).show();
+		$projectsItem.not($curr).hide();
+		let countItem = $('.projects__projects-list').find('.projects__list-item:visible').length;
+		if (countItem < 6) {
+			$('#load-more').hide();
+		} else {
+			$('#load-more').show();
+		}
+  }).filter('.active').click();
+
+	$("#load-more").click( function(event) {
+		event.preventDefault();
+		$curr.filter(':hidden').slice(0, 6).slideDown();
+		if ($curr.filter(':hidden').length == 0) {
+			$("#load-more").hide();
+			$('#view-less').fadeIn();
+		};
+	});
+
+	$("#view-less").click( function(event) {
+		event.preventDefault();
+		$curr.slice(6).slideUp();
+		$("#load-more").fadeIn();
+		$('#view-less').hide();;
+	});
+	
+	// 1. добавить возможность сворачивать viewLess
+
+	// // Load more section projects
+
+  // $("#load-more").click( function(event) {
+  //   event.preventDefault();
+  //   $(".projects__list-item:hidden").slice(0, 3).slideDown();
+  //   if ($(".projects__list-item:hidden").length == 0) {
+	// 		$("#load-more").html('Roll up').addClass('projects__roll-up');
+  //     $("#load-more").click( function() {
+	// 			$(".projects__list-item").slice(6).slideUp();
+	// 			$("#load-more").html('Load more').removeClass('projects__roll-up');
+	// 		});
+  //   };
+	// });
 });
